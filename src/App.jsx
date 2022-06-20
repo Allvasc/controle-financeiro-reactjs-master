4a56cc75c56ab590d3a3176488143389e7333bbe
Allison Vasconcelos <ah.vasconcelos@gmail.com>
2022-06-19 23:54:07 -0300

    criação de grid e gridItem

--- src/App.jsx
+++ src/App.jsx
@@ -5,56 +5,54 @@ import Form from './components/Form'
 import { useEffect, useState } from 'react'
 
 
-function App() {
-
-
- const data = localStorage.getItem("transactions")
-
-  const [transactionslist, setTransactionsList] = useState(
+const App = () => {
+  const data = localStorage.getItem("transactions");
+  const [transactionsList, setTransactionsList] = useState(
     data ? JSON.parse(data) : []
   );
-
-  const [income, setIncome] = useState(0)
-  const [expense, setExpense] = useState(0)
-  const [total, setTotal] = useState(0)
+  const [income, setIncome] = useState(0);
+  const [expense, setExpense] = useState(0);
+  const [total, setTotal] = useState(0);
 
   useEffect(() => {
+    const amountExpense = transactionsList
+      .filter((item) => item.expense)
+      .map((transaction) => Number(transaction.amount));
 
-    const amoutExpense = transactionslist
-    .filter((item) => item.expense)
-    .map((transaction) => Number(transaction.amout))
+    const amountIncome = transactionsList
+      .filter((item) => !item.expense)
+      .map((transaction) => Number(transaction.amount));
 
-    const amoutIncome = transactionslist
-    .filter((item) => !item.expense)
-    .map((transaction) => Number(transaction.amout))
+    const expense = amountExpense.reduce((acc, cur) => acc + cur, 0).toFixed(2);
+    const income = amountIncome.reduce((acc, cur) => acc + cur, 0).toFixed(2);
 
-    const expense = amoutExpense.reduce((acc, cur) => acc + cur, 0).toFixed(2)
-    const income = amoutIncome.reduce((acc, cur) => acc + cur, 0).toFixed(2)
+    const total = Math.abs(income - expense).toFixed(2);
 
-    const total = Math.abs(income - expense).toFixed(2)
+    setIncome(`R$ ${income}`);
+    setExpense(`R$ ${expense}`);
+    setTotal(`${Number(income) < Number(expense) ? "-" : ""}R$ ${total}`);
+  }, [transactionsList]);
 
-    setIncome(`R$ ${income}`)
-    setExpense(`R$ ${expense}`)
-    setTotal(`${Number(income) < Number(expense) ? "-" : ""}R$ ${total}`)
-  }, [transactionslist])
+  const handleAdd = (transaction) => {
+    const newArrayTransactions = [...transactionsList, transaction];
 
-  const handleAdd = (transaction) =>{
-    const newArrayTransactions = [...transactionslist, transaction]
+    setTransactionsList(newArrayTransactions);
 
-    setTransactionsList(newArrayTransactions)
-    
-    localStorage.setItem("transactions", JSON.stringify(newArrayTransactions))
-  }
-
+    localStorage.setItem("transactions", JSON.stringify(newArrayTransactions));
+  };
 
   return (
     <>
-      <Header/>
-      <Resume income={income} expense={expense} total={total}/>
-      <Form handleAdd={handleAdd}/>
+      <Header />
+      <Resume income={income} expense={expense} total={total} />
+      <Form
+        handleAdd={handleAdd}
+        transactionsList={transactionsList}
+        setTransactionsList={setTransactionsList}
+      />
       <GlobalStyle />
     </>
   );
-}
+};
 
 export default App;
--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -1,74 +1,73 @@
 import { useState } from 'react'
+import Grid from "../Grid";
 import * as C from './styles'
 
-const Form = ({handleAdd}) => {
+const Form = ({ handleAdd, transactionsList, setTransactionsList }) => {
+  const [desc, setDesc] = useState("");
+  const [amount, setAmount] = useState("");
+  const [isExpense, setExpense] = useState(false);
 
-  const [desc, setDesc] = useState("")
-  const [amount, setAmount] = useState("")
-  const [isExpense, setsIsExpense] = useState(false)
-
-  const generateID = () => Math.round(Math.random() * 1000)
+  const generateID = () => Math.round(Math.random() * 1000);
 
   const handleSave = () => {
     if (!desc || !amount) {
-      alert('informe a descrição e o valor')
+      alert("Informe a descrição e o valor!");
       return;
     } else if (amount < 1) {
-      alert('O valor deve ser positivo')
+      alert("O valor tem que ser positivo!");
       return;
     }
+
     const transaction = {
       id: generateID(),
       desc: desc,
       amount: amount,
-      expense: isExpense, 
-    }
+      expense: isExpense,
+    };
 
-    handleAdd(transaction)
+    handleAdd(transaction);
 
-    setDesc("")
-    setAmount("")
-
-  }
-
-
+    setDesc("");
+    setAmount("");
+  };
 
   return (
-    <C.Container>
-
-      <C.InputContent>
-        <C.Label>Descrição</C.Label>
-        <C.Input value={desc} onChange={(e) => setDesc(e.target.value)} />
-      </C.InputContent>
-
-      <C.InputContent>
-        <C.Label>Valor</C.Label>
-        <C.Input value={amount} type="number" onChange={(e) => setAmount(e.target.value)} />
-      </C.InputContent>
-
-      <C.RadioGroup>
-        <C.Input 
-          type="radio"
-          id="rIncome"
-          defaultChecked
-          name="group1"
-          onChange={(e) => setsIsExpense(!isExpense)}
+    <>
+      <C.Container>
+        <C.InputContent>
+          <C.Label>Descrição</C.Label>
+          <C.Input value={desc} onChange={(e) => setDesc(e.target.value)} />
+        </C.InputContent>
+        <C.InputContent>
+          <C.Label>Valor</C.Label>
+          <C.Input
+            value={amount}
+            type="number"
+            onChange={(e) => setAmount(e.target.value)}
           />
-        <C.Label htmlFor='rIncome'>Entrada</C.Label>
-
-        <C.Input 
-          type="radio"
-          id="rExpenses"
-          defaultChecked
-          name="group1"
-          onChange={(e) => setsIsExpense(!isExpense)}
+        </C.InputContent>
+        <C.RadioGroup>
+          <C.Input
+            type="radio"
+            id="rIncome"
+            defaultChecked
+            name="group1"
+            onChange={() => setExpense(!isExpense)}
           />
-        <C.Label htmlFor='rExpenses'>Saida</C.Label>
-      </C.RadioGroup>
-      <C.Button onClick={handleAdd}>Adicionar</C.Button>
-    </C.Container >
+          <C.Label htmlFor="rIncome">Entrada</C.Label>
+          <C.Input
+            type="radio"
+            id="rExpenses"
+            name="group1"
+            onChange={() => setExpense(!isExpense)}
+          />
+          <C.Label htmlFor="rExpenses">Saída</C.Label>
+        </C.RadioGroup>
+        <C.Button onClick={handleSave}>ADICIONAR</C.Button>
+      </C.Container>
+      <Grid itens={transactionsList} setItens={setTransactionsList} />
+    </>
+  );
+};
 
-  )
-}
-
-export default Form
+export default Form;
